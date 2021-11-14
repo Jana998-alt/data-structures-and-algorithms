90bf6a361206c9e63904bae367884978d26c2cd8
Jana Abdelkader <janaosama1300@gmail.com>
2021-11-14 08:18:06 +0200

    creating methods for pseudo queue

---
+++ python/stack-queue-pseudo/stack_queue_pseudo/stack_queue_pseudo.py
@@ -0,0 +1,93 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class Stack:
+    def __init__(self, node=None):
+        self.top = node
+
+    def push(self, value):
+        temp_node = self.top
+        self.top = Node(value)
+        self.top.next = temp_node
+
+    def pop(self):
+
+        if self.top == None:
+            raise ValueError('Empty Stack')
+
+        temp_node = self.top.next
+        temp_value = self.top.value
+        self.top = None
+        self.top = temp_node
+        return temp_value
+
+    def peek(self):
+
+        if self.top == None:
+            raise ValueError('Empty Stack')
+        else:
+            return self.top.value
+
+    def is_empty(self):
+        if self.top == None:
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        str = 'top ->'
+        current_node = self.top
+
+        while current_node:
+            print(current_node.value)
+            str = str + f" {current_node.value} ->"
+            if current_node == None:
+                str = str + ' NULL'
+                break
+            current_node = current_node.next
+
+        if current_node == None:
+            str = str + ' NULL'
+
+        return str
+
+
+
+# Methods:
+# enqueue
+# Arguments: value
+# Inserts value into the PseudoQueue, using a first-in, first-out approach.
+# dequeue
+# Arguments: none
+# Extracts a value from the PseudoQueue, using a first-in, first-out approach.h
+# NOTE: The Stack instances have only push, pop, and peek methods. You should use your own Stack implementation. Instantiate these Stack objects in your PseudoQueue constructor.
+
+
+class PseudoQueue():
+    def __init__(self):
+        self.stack_front = Stack()
+        self.stack_rear = Stack()
+
+    def enqueue(self, value):
+        self.stack_front.push(value)
+
+    def dequeue(self):
+        empty_stack = self.stack_rear
+        current = self.stack_front.head
+        
+        while current.next.next != None:
+            value = self.stack_front.peek()
+            self.stack_rear.push(value)
+            self.stack_front.pop()
+            current = current.next
+
+        self.stack_front = self.stack_rear
+        self.stack_rear = empty_stack
+
+
+
+if __name__ == "__main__":
+    pass
