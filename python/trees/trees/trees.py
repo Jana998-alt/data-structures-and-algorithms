31d946ab28e756d67eae15acc3c075c513110d36
Jana Abdelkader <janaosama1300@gmail.com>
2021-11-21 20:37:53 +0200

    modifications

--- python/trees/trees/trees.py
+++ python/trees/trees/trees.py
@@ -2,136 +2,128 @@ import re
 
 
 class Node:
-  def __init__(self, value):
-    self.value = value
-    self.right = None
-    self.left = None
+    def __init__(self, value):
+        self.value = value
+        self.right = None
+        self.left = None
 
-  def add_right(self, val):
-    self.right = Node(val)
+    def add_right(self, val):
+        self.right = Node(val)
 
-  def add_left(self, val):
-    self.left = Node(val)
+    def add_left(self, val):
+        self.left = Node(val)
 
 
 class BinaryTree:
-  def __init__(self, root=None):
-    if root != None:
-      self.root = Node(root)
-    else:
-      self.root = root
-    self.traversals_array = []
+    def __init__(self, root=None):
+        if root != None:
+            self.root = Node(root)
+        else:
+            self.root = root
+        self.traversals_array = []
 
-  # root >> left >> right
-  def depth_pre_order(self, root=None):
+    # root >> left >> right
+    def depth_pre_order(self, root=None):
 
-    if root == None:
-      root = self.root
-      self.traversals_array = []
-      self.traversals_array.append(root.value)
+        if root == None:
+            root = self.root
+            self.traversals_array = []
+            self.traversals_array.append(root.value)
 
-    if root.left != None:
-      self.traversals_array.append(root.left.value)
-      self.depth_pre_order(root.left)
+        if root.left != None:
+            self.traversals_array.append(root.left.value)
+            self.depth_pre_order(root.left)
 
-    if root.right != None:
-      self.traversals_array.append(root.right.value)
-      self.depth_pre_order(root.right)
+        if root.right != None:
+            self.traversals_array.append(root.right.value)
+            self.depth_pre_order(root.right)
 
-    return self.traversals_array
+        return self.traversals_array
 
-  # left >> root >> right
+    # left >> root >> right
 
-  def depth_in_order(self):
+    def depth_in_order(self):
 
-    root = self.root
-    traversals_array = []
+        root = self.root
+        traversals_array = []
 
-    def _inorder(root):
+        def _inorder(root):
 
-      if root.left != None:
-        _inorder(root.left)
+            if root.left != None:
+                _inorder(root.left)
 
+            traversals_array.append(root.value)
 
-      traversals_array.append(root.value)
+            if root.right != None:
+                _inorder(root.right)
 
-      if root.right != None:
-        _inorder(root.right)
+        _inorder(root)
 
+        return traversals_array
 
+    def depth_post_order(self, root=None):
 
-    _inorder(root)
+        if root == None:
+            root = self.root
+            self.traversals_array = []
 
-    return traversals_array
+        if root:
+            if root.left != None:
+                self.depth_post_order(root.left)
 
-  def depth_post_order(self, root=None):
+            if root.right != None:
+                self.depth_post_order(root.right)
 
-    if root == None:
-      root = self.root
-      self.traversals_array = []
+            self.traversals_array.append(root.value)
 
-    if root:
-      if root.left != None:
-        self.depth_post_order(root.left)
-
-
-      if root.right != None:
-        self.depth_post_order(root.right)
-
-
-      self.traversals_array.append(root.value)
-
-    return self.traversals_array
+        return self.traversals_array
 
 
 class BinarySearchTree:
-  def __init__(self) -> None:
-      self.super(BinaryTree)
+    def __init__(self) -> None:
+        self.super(BinaryTree)
 
-  def add(self, value, root=None):
-    if root == None:
-      self.root = Node(value)
+    def add(self, value, root=None):
+        if root == None:
+            self.root = Node(value)
 
-    is_larger = (root.value > value)
+        is_larger = (root.value > value)
 
-    if root.right == None and is_larger:
-      root.right = Node(value)
+        if root.right == None and is_larger:
+            root.right = Node(value)
 
-    else:
-      self.add(value, root.right)
+        else:
+            self.add(value, root.right)
 
-    if root.left == None and not is_larger:
-      root.left = Node(value)
-    else:
-      self.add(value, root.left)
+        if root.left == None and not is_larger:
+            root.left = Node(value)
+        else:
+            self.add(value, root.left)
 
-  def contains(self, val, root=None):
+    def contains(self, val, root=None):
 
-    if root == None:
-      root = self.root
-      if root == None:
-        return False
+        if root == None:
+            root = self.root
+            if root == None:
+                return False
 
-    if root.value == val:
-      return True
+        if root.value == val:
+            return True
 
-    is_larger = (root.value > val)
+        is_larger = (root.value > val)
 
-    if is_larger:
-      if root.right != None:
-        self.contains(val, root.right)
-      else:
-        return False
+        if is_larger:
+            if root.right != None:
+                self.contains(val, root.right)
+            else:
+                return False
 
-
-    if not is_larger:
-      if root.left != None:
-        self.contains(val, root.left)
-      else:
-        return False
-
-
+        if not is_larger:
+            if root.left != None:
+                self.contains(val, root.left)
+            else:
+                return False
 
 
 if __name__ == "__main__":
-  pass
+    pass
